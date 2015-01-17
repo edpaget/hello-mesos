004f1816e1166f5738bdfdc3e85766f4cadbfa01
Edward Paget <edward@cassetteta.pe>
2015-01-17 10:12:13 -0600

    Initial Commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject hello-mesos "0.1.0-SNAPSHOT"
+  :description "TODO"
+  :url "TODO"
+  :license {:name "TODO: Choose a license"
+            :url "http://choosealicense.com/"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.7"]]
+                   :source-paths ["dev"]}})
